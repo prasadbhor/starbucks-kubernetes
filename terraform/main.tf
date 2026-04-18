d24d932fc645eab3d1ff11c690c6f1533594123f
Prasad Bhor <129607062+prasadbhor@users.noreply.github.com>
2026-04-18 22:32:52 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,7 +42,7 @@ resource "aws_security_group" "ec2_security_group" {
 
 resource "aws_instance" "Monitoring_server" {
 ami = "ami-00bb6a80f01f03502"  
-instance_type = "t2.medium"
+instance_type = "c7i-flex.large"
 security_groups = [aws_security_group.ec2_security_group.name]
 key_name = var.key_name
 tags = {
